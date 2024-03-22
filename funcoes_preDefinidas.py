89ac6c287d0ac25e34b1bcec466f79289513cf62
EDUARDA NUNES MANENTI <eduarda_manenti@estudante.sc.senai.br>
2024-03-21 22:08:56 -0300

    Iniciado estudos em python. Entendendo funções matemáticas.

---
+++ funcoes_preDefinidas.py
@@ -0,0 +1,17 @@
+# Funções pré-definidas
+
+# Valor absoluto
+print("Função ABS em um número negativo: ", abs(-19))
+print("Função ABS em um número positivo: ", abs(89))
+
+# Potência
+print("Função pow em um número 3 elevado a 3: ", pow(3, 3))
+print("Função pow em um número 9 elevado a 0.5: ", pow(9, 0.5))
+
+# Arredondamento
+print("Função round em um número 45.76 arredondado para 1 casa: ", round(45.76, 1))
+print("Função round em um número 3.56788 arredondado para 2 casas: ", round(3.56788, 2))
+
+# Valor máximos e mínimos
+print("Função min nos números [4, 6]: ", min(4, 6))
+print("Função max nos números [10, 9]: ", max(10, 9))
---
+++ soma.py
@@ -0,0 +1,11 @@
+numero1 = 1
+numero2 = 5
+
+soma = numero1 + numero2
+print('A soma é: ', soma)
+
+
+
+
+
+
